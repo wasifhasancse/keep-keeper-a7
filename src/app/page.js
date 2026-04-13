f045fffc5dbeef139e0ef10f584c12aa2c6972e3
Wasif Hasan <wasifhasancse@gmail.com>
2026-04-13 21:31:11 +0600

    develop the Banner section with responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="hero bg-base-200 min-h-screen max-w-11/12 mx-auto">
-      <div className="hero-content text-center">
+    <div className="hero bg-base-200 min-h-[50vh]">
+      <div className="hero-content text-center max-w-11/12 mx-auto">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-5xl font-bold">
             Friends to keep close in your life
@@ -13,7 +13,7 @@ export default function Home() {
             Your personal shelf of meaningful connections. Browse, tend, and
             nurture the relationships that matter most.
           </p>
-          <Link href={'/'}>
+          <Link href={"/"}>
             <button className="px-5 py-2.5 text-lg font-medium rounded-md bg-green-300 hover:bg-green-200 hover:text-green-700 border-2 border-green-500">
               + Add a Friend
             </button>

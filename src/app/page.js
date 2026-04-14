6a34aaa4e27e7b562b436456bb38c2391f10defd
Wasif Hasan <wasifhasancse@gmail.com>
2026-04-14 15:36:04 +0600

    develop the friends details page

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ import { readFile } from "node:fs/promises";
 import path from "node:path";
 
 export default async function Home() {
-  // const filePath = path.join(process.cwd(), "public", "Data", "friends.json");
-  // const friendsData = JSON.parse(await readFile(filePath, "utf-8"));
-  const friendsData = await fetch("http://localhost:3000/Data/friends.json").then(res=>res.json());
+  const filePath = path.join(process.cwd(), "public", "Data", "friends.json");
+  const friendsData = JSON.parse(await readFile(filePath, "utf-8"));
+  // const friendsData = await fetch("/Data/friends.json").then(res=>res.json());
   return (
     <main className="bg-slate-50">
       <section>

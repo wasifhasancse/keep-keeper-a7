4990c8ca16525e525d531ff95a5814ff8cdd4cfd
Wasif Hasan <wasifhasancse@gmail.com>
2026-04-15 19:58:38 +0600

    change the search box input type

--- src/Components/TimeLine/Searching.jsx
+++ src/Components/TimeLine/Searching.jsx
@@ -9,7 +9,7 @@ const Searching = () => {
     <div className="relative">
       <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-zinc-400 z-10 pointer-events-none" />
       <input
-        type="text"
+        type="search"
         placeholder="Search by friend or interaction type"
         className="input input-bordered w-full bg-zinc-50 border-zinc-200 pl-9 focus:outline-none focus:border-green-400"
         onChange={manageSearch}

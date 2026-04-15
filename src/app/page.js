49072e44923b5ce3f48586de565d68f1eebcb2c0
Wasif Hasan <wasifhasancse@gmail.com>
2026-04-15 15:17:16 +0600

    set dynamic value in home page stats section

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,11 @@ import { readFile } from "node:fs/promises";
 import path from "node:path";
 
 export default async function Home() {
- const filePath = path.join(process.cwd(), "public", "Data", "friends.json");
- const friendsData = JSON.parse(await readFile(filePath, "utf-8"));
-// const res = await fetch("live-link/data.json", {
-//   cache: "no-store",
-// });
+  const filePath = path.join(process.cwd(), "public", "Data", "friends.json");
+  const friendsData = JSON.parse(await readFile(filePath, "utf-8"));
+  // const res = await fetch("live-link/data.json", {
+  //   cache: "no-store",
+  // });
   // const friendsData = await fetch("/Data/friends.json").then(res=>res.json());
   return (
     <main className="bg-slate-50">
@@ -36,9 +36,22 @@ export default async function Home() {
 
       <section className="max-w-11/12 mx-auto my-10 grid md:grid-cols-2 lg:grid-cols-4 gap-5">
         <Stats title={"Total Friends"} value={friendsData.length} />
-        <Stats title={"On Track"} value={""} />
-        <Stats title={"Need Attention"} value={""} />
-        <Stats title={"Interactions This Month"} value={""} />
+        <Stats
+          title={"On Track"}
+          value={friendsData.filter((item) => item.status == "on-track").length}
+        />
+        <Stats
+          title={"Need Attention"}
+          value={
+            friendsData.filter((item) => item.days_since_contact > 30).length
+          }
+        />
+        <Stats
+          title={"Interactions This Month"}
+          value={
+            friendsData.filter((item) => item.days_since_contact < 30).length
+          }
+        />
       </section>
 
       <div className="divider max-w-11/12 mx-auto my-16"></div>
